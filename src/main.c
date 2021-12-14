06b5160a25897d24602b5be834a760fc5bb3752a
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2021-12-14 14:47:20 +0100

    Add exit at the end

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/14 14:21:58 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2021/12/14 14:41:46 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2021/12/14 14:44:33 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int main(int argc, char const *argv[])
     SHUT(argc);
     SHUT(argv);
     
-    return 0;
+    exit(EXIT_SUCCESS);
 }
