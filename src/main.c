cb359669f18827ecc765e9f365a4b408a9932178
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2021-12-14 14:56:11 +0100

    Add some constructors

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/14 14:21:58 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2021/12/14 14:44:33 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2021/12/14 14:49:20 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/coldrace.h"
+#include "coldrace.h"
 
 int main(int argc, char const *argv[])
 {
---
+++ src/utils/constructors.c
@@ -0,0 +1,33 @@
+#include "coldrace.h"
+
+/**
+ * Creates a new pair.
+ * 
+ * @param key 
+ * @param value 
+ * @return t_pair* 
+ */
+t_pair* new_pair(char* key, char* value)
+{
+	t_pair* output = malloc(sizeof(t_pair));
+	output->key = key;
+	output->value = value;
+	return (output);
+}
+
+/**
+ * Creates a new node.
+ * 
+ * @param pair 
+ * @param left 
+ * @param right 
+ * @return t_node* 
+ */
+t_node* new_node(t_pair* pair, t_node* left, t_node* right)
+{
+	t_node* output = malloc(sizeof(t_node));
+	output->pair = pair;
+	output->left = left;
+	output->right = right;
+	return (output);
+}
